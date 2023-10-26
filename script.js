b0b527d3ec3a96557e4e28679700f51694b42102
felipe <130618161+fa9958189@users.noreply.github.com>
2023-10-26 17:54:48 -0300

    c
    c

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
- 
+        const photoContainer = document.querySelector('.photo-container');
+        const popup = document.getElementById('myPopup');
+        const closePopup = document.getElementById('closePopup');
+        const closeButton = document.getElementById('closeButton');
+
+        photoContainer.addEventListener('click', () => {
+            popup.style.display = 'block';
+        });
+
+        closePopup.addEventListener('click', () => {
+            // Evite fechar o popup quando clicar em closePopup (X)
+            event.stopPropagation();
+        });
+
+        closeButton.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+
+        // Feche o popup se o usuário clicar fora da imagem
+        window.addEventListener('click', (event) => {
+            if (event.target === popup) {
+                popup.style.display = 'none';
+            }
+        });
+
+
+
        function toggleSidebar() {
         var sidebar = document.querySelector('.sidebar');
         sidebar.classList.toggle('open-menu');
@@ -32,57 +58,3 @@
             window.location.href = linkInstagram;
     }
 
-
-
-
-
-                const photoContainer = document.querySelector('.photo-container');
-            const popup = document.getElementById('myPopup');
-            const closePopup = document.getElementById('closePopup');
-
-            photoContainer.addEventListener('click', () => {
-                popup.style.display = 'block';
-            });
-
-            closePopup.addEventListener('click', () => {
-                popup.style.display = 'none';
-            });
-
-            // Feche o popup se o usuário clicar fora da imagem
-            window.addEventListener('click', (event) => {
-                if (event.target === popup) {
-                    popup.style.display = 'none';
-                }
-            });
-                   // Fechar o popup quando o botão de fechar for clicado
-                   closePopupButton.addEventListener('click', () => {
-                    closeImagePopup();
-                });
-
-
-
-            // // Adicionar eventos de clique às imagens
-            //     imagePopups.forEach(image => {
-            //         image.addEventListener('click', () => {
-            //             const title = image.getAttribute('data-title');
-            //             const description = image.getAttribute('data-description');
-            //             openImagePopup(title, description);
-            //         });
-            //     });
-
-            //     // Abrir o popup quando o botão for clicado
-            //     openPopupButton.addEventListener('click', () => {
-            //         popup.style.display = 'block';
-            //     });
-
-            //     // Fechar o popup quando o botão de fechar for clicado
-            //     closePopupButton.addEventListener('click', () => {
-            //         closeImagePopup();
-            //     });
-
-            //     // Fechar o popup quando o usuário clicar fora dele
-            //     window.addEventListener('click', (event) => {
-            //         if (event.target === popup) {
-            //             closeImagePopup();
-            //         }
-            //     });
